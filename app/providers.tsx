07905f801111a182c745b096f82e4b982a15e043
com/ryo7400s-del <ryo7400.s@gmail.com>
2026-06-26 14:54:23 +0900

    fix: move PrivyProvider to client component

---
+++ app/providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { PrivyWalletProvider } from "@/lib/privy/PrivyWalletContext";
+
+const arcTestnet = {
+  id: 5042002,
+  name: "Arc Testnet",
+  nativeCurrency: { name: "USDC", symbol: "USDC", decimals: 18 },
+  rpcUrls: { default: { http: ["https://rpc.testnet.arc.network"] } },
+} as const;
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      config={{
+        loginMethods: ["google"],
+        appearance: { theme: "dark" },
+        embeddedWallets: { ethereum: { createOnLogin: "all-users" } },
+        defaultChain: arcTestnet,
+        supportedChains: [arcTestnet],
+      }}
+    >
+      <PrivyWalletProvider>
+        {children}
+      </PrivyWalletProvider>
+    </PrivyProvider>
+  );
+}
